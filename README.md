dd70f5ef3a06d360553ea92772c610a70db1b830
lsonnino <lorenzo@sonnino.com>
2020-04-06 17:03:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# language-oz-2
+ An updated version of the language-oz package for Atom
---
+++ spec/language-oz-2-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageOz2View from '../lib/language-oz-2-view';
+
+describe('LanguageOz2View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
